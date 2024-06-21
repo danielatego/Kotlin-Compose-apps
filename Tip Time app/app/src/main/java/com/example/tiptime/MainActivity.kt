7b45215227c4c1d858dda0229e51d93858bfc400
danielatego <danielatego75@gmail.com>
2024-06-21 10:44:22 +0300

    complete tipping app

--- Tip Time app/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Tip Time app/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -184,7 +185,8 @@ fun RoundTheTipRow(
  * according to the local currency.
  * Example would be "$10.00".
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUP: Boolean): String {
+@VisibleForTesting
+internal fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUP: Boolean): String {
     var tip = tipPercent / 100 * amount
     if (roundUP) {
         tip = kotlin.math.ceil(tip)
---
+++ Tip Time app/app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -0,0 +1,20 @@
+package com.example.tiptime
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import java.text.NumberFormat
+
+class TipCalculatorTests {
+
+    @Test
+    fun calculateTip_20PercentNoRoundup() {
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(
+            amount = amount,
+            tipPercent = tipPercent,
+            roundUP = false)
+        assertEquals(expectedTip,actualTip)
+    }
+}
