f140b43ff877679e39328b7dcff69e33d10ead16
danielatego <danielatego75@gmail.com>
2024-07-01 07:29:41 +0300

    Unscramble game

--- Unscramble/app/src/main/java/com/example/unscramble/ui/GameScreen.kt
+++ Unscramble/app/src/main/java/com/example/unscramble/ui/GameScreen.kt
@@ -83,9 +83,11 @@ fun GameScreen(
         )
         GameLayout(
             userGuess = gameViewModel.userGuess,
+            wordCount = gameUiState.currentWordCount,
             onUserGuessChanged = {gameViewModel.updateUserGuess(it)},
-            onKeyboardDone = {},
-            currentScrambledWord = gameUiState.currentScrambleWord,
+            onKeyboardDone = { gameViewModel.checkUserGuess() },
+            currentScrambledWord = gameUiState.currentScrambledWord,
+            isGuessWrong = gameUiState.isGuessedWordWrong,
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
@@ -101,7 +103,7 @@ fun GameScreen(
 
             Button(
                 modifier = Modifier.fillMaxWidth(),
-                onClick = { }
+                onClick = { gameViewModel.checkUserGuess() }
             ) {
                 Text(
                     text = stringResource(R.string.submit),
@@ -110,7 +112,7 @@ fun GameScreen(
             }
 
             OutlinedButton(
-                onClick = { },
+                onClick = { gameViewModel.skipWord() },
                 modifier = Modifier.fillMaxWidth()
             ) {
                 Text(
@@ -119,13 +121,18 @@ fun GameScreen(
                 )
             }
         }
-
-        GameStatus(score = 0, modifier = Modifier.padding(20.dp))
+        if (gameUiState.isGameOver) {
+            FinalScoreDialog(score = gameUiState.score, onPlayAgain = { gameViewModel.resetGame() })
+        }
+        GameStatus(score = gameUiState.score, modifier = Modifier.padding(20.dp))
     }
 }
 
 @Composable
-fun GameStatus(score: Int, modifier: Modifier = Modifier) {
+fun GameStatus(
+    score: Int,
+    modifier: Modifier = Modifier
+) {
     Card(
         modifier = modifier
     ) {
@@ -141,7 +148,9 @@ fun GameStatus(score: Int, modifier: Modifier = Modifier) {
 fun GameLayout(
     onUserGuessChanged:(String) -> Unit,
     userGuess: String,
+    isGuessWrong: Boolean,
     onKeyboardDone:() -> Unit,
+    wordCount: Int,
     currentScrambledWord: String,
     modifier: Modifier = Modifier) {
     val mediumPadding = dimensionResource(R.dimen.padding_medium)
@@ -161,7 +170,7 @@ fun GameLayout(
                     .background(colorScheme.surfaceTint)
                     .padding(horizontal = 10.dp, vertical = 4.dp)
                     .align(alignment = Alignment.End),
-                text = stringResource(R.string.word_count, 0),
+                text = stringResource(R.string.word_count, wordCount),
                 style = typography.titleMedium,
                 color = colorScheme.onPrimary
             )
@@ -185,8 +194,8 @@ fun GameLayout(
                     disabledContainerColor = colorScheme.surface,
                 ),
                 onValueChange = onUserGuessChanged,
-                label = { Text(stringResource(R.string.enter_your_word)) },
-                isError = false,
+                label = { if(isGuessWrong) Text( stringResource(R.string.wrong_guess)) else Text(stringResource(R.string.enter_your_word)) },
+                isError = isGuessWrong,
                 keyboardOptions = KeyboardOptions.Default.copy(
                     imeAction = ImeAction.Done
                 ),
--- Unscramble/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ Unscramble/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -4,6 +4,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.MAX_NO_OF_WORDS
+import com.example.unscramble.data.SCORE_INCREASE
 import com.example.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -43,10 +45,31 @@ class GameViewModel : ViewModel() {
         }
         return String(tempWord)
     }
-
+    private fun updateGameState(updatedScore: Int){
+       if (usedWords.size == MAX_NO_OF_WORDS){
+           //Last round in the game, update isGameOver to true, don't pick a new word
+           _uiState.update {currentState->
+               currentState.copy(
+                   isGuessedWordWrong = false,
+                   score = updatedScore,
+                   isGameOver = true,
+               )
+           }
+    } else {
+        // Normal round in the game
+        _uiState.update { currentState ->
+           currentState.copy(
+               isGuessedWordWrong = false,
+               currentScrambledWord = pickRandomWordAndShuffle(),
+               score = updatedScore,
+               currentWordCount = currentState.currentWordCount.inc(),
+           )
+        }
+       }
+    }
     fun resetGame() {
         usedWords.clear()
-        _uiState.value = GameUiState(currentScrambleWord = pickRandomWordAndShuffle())
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
     }
 
     fun updateUserGuess(guessedWord: String){
@@ -55,15 +78,23 @@ class GameViewModel : ViewModel() {
 
     fun checkUserGuess() {
         if(userGuess.equals(currentWord, ignoreCase = true)){
-
+            //User's guess is correct, increase the score
+            //and call updateGameState() to prepare the game for the next round
+            val updatedScore = _uiState.value.score.plus(SCORE_INCREASE)
+            updateGameState(updatedScore)
         } else{
             //User's guess is wrong, show and error
-            _uiState.update {
-
+            _uiState.update { currentState->
+                currentState.copy(isGuessedWordWrong = true)
             }
 
         }
         //Reset user guess
+        updateUserGuess("")
+    }
+    fun skipWord() {
+        updateGameState(_uiState.value.score)
+        // Reset user guess
         updateUserGuess("")
     }
 
