6571a7d0434627dbbb4079eec322f023b34a3490
danielatego <danielatego75@gmail.com>
2024-07-03 11:18:51 +0300

    tests

--- Cupcake/app/build.gradle.kts
+++ Cupcake/app/build.gradle.kts
@@ -82,6 +82,19 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.navigation:navigation-compose:2.7.4")
 
+//    androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
+//    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+//    androidTestImplementation("androidx.navigation:navigation-testing:2.7.7")
+//    androidTestImplementation("androidx.test.espresso:espresso-intents:3.6.1")
+//    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.05.01"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation("androidx.navigation:navigation-testing:2.6.0")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("org.testng:testng:6.9.6")
+
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
