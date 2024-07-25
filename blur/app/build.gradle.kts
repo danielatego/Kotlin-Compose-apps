6c213e8c85ef1e0ab93efe6282891ac7115fbf6f
danielatego <danielatego75@gmail.com>
2024-07-25 11:13:56 +0300

    background services workers and tests

--- blur/app/build.gradle.kts
+++ blur/app/build.gradle.kts
@@ -86,6 +86,16 @@ dependencies {
     // WorkManager dependency
     implementation("androidx.work:work-runtime-ktx:2.9.0")
 
+
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    // Testing
+
+    // Espresso
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    // Junit
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    // Work testing
+    androidTestImplementation("androidx.work:work-testing:2.9.0")
 }
